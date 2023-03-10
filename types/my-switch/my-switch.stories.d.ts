ccf0c2c9cbe835a929f0b215c2d4693399ae4974
Burton Smith <burtonsmith@microsoft.com>
2023-03-10 12:06:29 -0500

    update text

--- types/my-switch/my-switch.stories.d.ts
+++ types/my-switch/my-switch.stories.d.ts
@@ -2,7 +2,12 @@ import "./index";
 declare const _default: {
     title: string;
     component: string;
-    args: {};
+    argTypes: {};
+    parameters: {
+        actions: {
+            handles: never[];
+        };
+    };
 };
 export default _default;
 export declare const Default: any;
